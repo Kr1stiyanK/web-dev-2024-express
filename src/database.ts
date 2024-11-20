6aaee02a5dd1d638602ad7782ad20913be351908
Kr1stiyanK <kriskar1@abv.bg>
2024-11-20 20:08:16 +0200

    done 2

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import { SubjectModel } from './subject/subject';
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: 'database.sqlite', // SQLite database file path
+    storage: 'database.sqlite',
 });
 
 export const db = {
@@ -27,3 +27,24 @@ db.models.University.hasMany(db.models.User, {
     foreignKey: 'universityId',
     as: 'users',
 });
+
+db.models.User.belongsToMany(db.models.Subject, {
+    through: 'UserSubjects',
+    as: 'subjects',
+    foreignKey: 'userId',
+  
+  });
+  
+  db.models.Subject.belongsToMany(db.models.User, {
+    through: 'UserSubjects',
+    as: 'users',
+    foreignKey: 'subjectId',
+    
+  });
+  
+
+  db.sequelize.sync({ force: false }).then(() => {
+    console.log('Database & tables created!');
+  }).catch((err) => {
+    console.error('Unable to create tables:', err);
+  });
